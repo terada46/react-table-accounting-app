b47d72762669c1a121475ff83a4678306ad84a40
terada46 <mizukiloveu@gmail.com>
2020-08-10 01:51:29 +0800

    Update createStore.js and App.test.js

--- src/stores/createStore.js
+++ src/stores/createStore.js
@@ -3,15 +3,14 @@ import React, { createContext, useContext, useState, useMemo } from 'react';
 const useApi = (apiFactory, initialState) => {
     let [ state, setState ] = useState(initialState);
     return useMemo(() => apiFactory({ state, setState }), [
-      state,
-      setState,
-      apiFactory
+        state,
+        setState,
+        apiFactory
     ]);
-  };
+};
   
-  const createStore = (apiFactory, initialState) => {
+const createStore = (apiFactory, initialState) => {
     const StoreContext = createContext();
-
     const StoreProvider = props => {
         let store = useApi(apiFactory, initialState);
         return (
@@ -24,8 +23,8 @@ const useApi = (apiFactory, initialState) => {
     let useStore = () => {
         return useContext(StoreContext);
     }
-    
-    return [ StoreProvider, useStore ]
-  };
 
-  export default createStore;
+    return [ StoreProvider, useStore ]
+};
+
+export default createStore;
